6de495121e070031d09fefc6ada2ae8008cf1864
TrySpace <info@cube.dj>
2014-10-15 15:02:31 +0200

    defining "undefined" type does not work; added console.time() to track each update
    something with tabs and spaces and sublimetext

--- ReactDebugMixin.js
+++ ReactDebugMixin.js
@@ -2,28 +2,25 @@ var ReactDebugMixin = {
 
   componentWillMount: function() {
   	this._consoleCheck();
-	this.debugUpdateCount = 0;
-	this._debugOn = false;
-
+	    this.debugUpdateCount = 0;
+	    time("MountTime")
   },
 
   debug: function(name, extra, collapse){
   	// name must be string
-  	this._debugOn = true;
   	if(typeof name === "undefined"){
   		err("ReactDebug: No name given: `this.debug(`componentName`, `stateKey`)`, defaulting to `componentName`")
   	}
   	this.componentName = name ? name : 'componentName';
-  	this.stateKey = extra ? extra : "undefined";
+  	this.stateKey = extra ? extra : "_x_EMPTY_x_";
   	this.collapsed = collapse ? true : false;
   	this.collapsed ? grpC(this.componentName+" Will Mount: ") : grp(this.componentName+" Will Mount: ")
-  	time("MountTime: "+this.componentName);
   },
 
   extra: function(){
-  	if (typeof this.stateKey !== "undefined"){
+  	if (this.stateKey !== "_x_EMPTY_x_"){
   		if (this.stateKey in this.state){
-  			nfo("Extra: ", this.state[this.stateKey])
+  			nfo(this.stateKey+":", this.state[this.stateKey])
   		} else {
   			throw new Error("ReactDebug: `"+this.stateKey+"` does not exist on this component");
   		}
@@ -31,59 +28,47 @@ var ReactDebugMixin = {
   },
 
   componentDidMount: function(){
-
-  	if(this._debugOn){
-	  	this.collapsed ? grpC(this.componentName+" Did Mount: ") : grp(this.componentName+" Did Mount: ")
-	  		this.debugmounted = true;
-
-	  		this.extra();
-	  	grpE(this.componentName+" Did Mount: ")
-	  	grpE(this.componentName+" Will Mount: ")
-	  	timeE("MountTime: "+this.componentName);
-	}
+  	this.collapsed ? grpC(this.componentName+" Did Mount: ") : grp(this.componentName+" Did Mount: ")
+  		this.debugmounted = true;
+  		timeE("MountTime")
+  		this.extra();
+  	grpE(this.componentName+" Did Mount: ")
+  	grpE(this.componentName+" Will Mount: ")
   },
 
   componentWillReceiveProps: function(nextProps) {
-  	if(this._debugOn){
-	  	this.collapsed ? grpC(this.componentName+" Will Receive Props: ") : grp(this.componentName+" Will Receive Props: ")
-	  		log("next Props: %O", nextProps)
-	  		this.extra();
-	 }
+  	this.collapsed ? grpC(this.componentName+" Will Receive Props: ") : grp(this.componentName+" Will Receive Props: ")
+  		log("next Props: %O", nextProps)
+  		this.extra();
   },
 
   componentWillUpdate: function(nextProps, nextState){
-  	if(this._debugOn){
-	  	this.collapsed ? grpC(this.componentName+" Will Update: ") : grp(this.componentName+" Will Update: ")
-	  		log("next Props: %O", nextProps)
-	  		log("next State: %O", nextState)
-	  		this.extra();
-		  	time("UpdateTime: "+this.componentName);
-	}
+  	this.collapsed ? grpC(this.componentName+" Will Update: ") : grp(this.componentName+" Will Update: ")
+  		log("next Props: %O", nextProps)
+  		log("next State: %O", nextState)
+  		this.extra();
+	  	time("UpdateTime")
   },
 
   componentDidUpdate: function(prevProps, prevState){
-  	if(this._debugOn){
-	  	grpE(this.componentName+" Will Receive Props: ")
-	  	grpE(this.componentName+" Will Update: ")
+  	grpE(this.componentName+" Will Receive Props: ")
+  	grpE(this.componentName+" Will Update: ")
 
-	  	this.collapsed ? grpC(this.componentName+" Did Update: ") : grp(this.componentName+" Did Update: ")
-	  		timeE("UpdateTime: "+this.componentName);
-		  	this.debugUpdateCount++;
-		    log("UpdateCounter: "+this.debugUpdateCount)
-		    log("previous Props: %O", prevProps)
-		    log("previous State: %O", prevState)
-		    this.extra();
-		grpE(this.componentName+" Did Update: ")
-	}
+  	this.collapsed ? grpC(this.componentName+" Did Update: ") : grp(this.componentName+" Did Update: ")
+  		timeE("UpdateTime")
+	  	this.debugUpdateCount++;
+	    log("UpdateCounter: "+this.debugUpdateCount)
+	    log("previous Props: %O", prevProps)
+	    log("previous State: %O", prevState)
+	    this.extra();
+	grpE(this.componentName+" Did Update: ")
   },
 
   componentWillUnmount: function() {
-  	if(this._debugOn){
-	  	this.collapsed ? grpC(this.componentName+" Will Unmount: ") : grp(this.componentName+" Will Unmount: ")
-	    	log("Total UpdateCounter: "+this.debugUpdateCount)
-	    	this.extra();
-	    grpE(this.componentName+" Will Unmount: ")
-	}
+  	this.collapsed ? grpC(this.componentName+" Will Unmount: ") : grp(this.componentName+" Will Unmount: ")
+    	log("Total UpdateCounter: "+this.debugUpdateCount)
+    	this.extra();
+    grpE(this.componentName+" Will Unmount: ")
   },
   _consoleCheck: function(){
   	// Avoid `console` errors in browsers that lack a console.
